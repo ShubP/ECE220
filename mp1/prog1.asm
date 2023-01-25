3d83d3a371a17a0300aca633810c4bf7757ccfc4
Shub Pereira <shubbp2@illinois.edu>
2023-01-25 15:42:35 -0600

    mp1 completed

--- mp1/prog1.asm
+++ mp1/prog1.asm
@@ -1,3 +1,4 @@
+; partners: mamir6, ykko2
 ;
 ; The code given to you here implements the histogram calculation that 
 ; we developed in class.  In programming lab, we will add code that
@@ -95,82 +96,97 @@ GET_NEXT
 	BRnzp COUNTLOOP		; go to start of counting loop
 
 
-
 PRINT_HIST
+
+; This part of the code is responsible for the printing of the histogram according 
+; to the data stored by the previous section of code. The histogram is printed line
+; by line. First the character '@' is printed to represent characters other than 
+; letters. A space is also printed. Then using the data stored in the corresponding 
+; address for the specific character, it is extracted bit by bit and printed in 
+; hexadecimal. This is repeated for characters A-Z and with their respective stored 
+; values at the memory location. The approach used for printing is checking if the 
+; extracted 4 bits represent either a number between 0-9 or greater (A-F). 
+; Accordingly, the bits act as an offset for printing from 0 or A in ASCII format.
+
+; table of register use in this part of the code
+;    R0 holds the extracted digit and also the output to printed to the display
+;    R1 holds the address for the current bin in the histogram
+;    R2 is the loop counter for the bins to be printed
+;    R3 holds a pointer to the current character in the histogram
+;    R4 holds the character count at the address given by R1
+;    R5 is the counter for digits that need to be printed for each character
+;    R6 holds the bit count for each digit and also the extracted digit as offset
+;
 	
 	LD R1, HIST_ADDR	; loads starting address of histogram into R1
 	LD R2, NUM_BINS 	; set R2 as loop counter for bins (27)
 	AND R3,R3,#0		; clears R3 which points to the current position
 
-
-
 BIN_CHAR
-	LD R0,CHAR_START	; loads R0 with the start position of chars(@)
+	LD R0,OFFSET		; loads R0 with the start position of chars(@)
 	ADD R0,R0,R3		; Sets R0 with current char as positioned by R3 
 	OUT			; prints the current character in R0 
 	LD R0,SPACE		; loads R0 with a space as per the formatting
 	OUT			; prints a space
 	
-BIN_VAL
+BIN_VALUE
 	LDR R4,R1,#0		; load R1 into R4, the info starting from x3F00
 	
 
 	AND R5, R5, #0		; clear R5
-	ADD R5, R5, #4		; sets R5, the hex digit counter	
-HEX_PRINT
+	ADD R5, R5, #4		; sets R5, the digit counter	
+DIGIT
 	AND R6, R6, #0		; clear R6
 	ADD R6, R6, #4		; sets R6, the individual bit counter
 	AND R0, R0, #0		; clear R0
 
-EXTRACT_LOOP	
+EXTRACT	
 	ADD R0, R0, R0		; left shift R0	
 	ADD R4, R4, #0		; clear 
-	BRzp SHIFT		;
-	ADD R0, R0, #1		;
+	BRzp SHIFT		; no bit is added since MSB is 0-bit
+	ADD R0, R0, #1		; adds 1 to R0 since MSB is 1-bit
 
 SHIFT	
-	ADD R4, R4, R4		;
-	ADD R6, R6, #-1		; Decrements bit counter
-	BRp EXTRACT_LOOP 	; loops back if bits remaining
+	ADD R4, R4, R4		; left shifts the histogram value
+	ADD R6, R6, #-1		; decrements bit counter
+	BRp EXTRACT		; loops back until first 4 digits are extracted
 
-HEX_CHECK
+HEX_PRINT
 	AND R6,R6, #0		; clear R6 for re-use
-	ADD R6, R6, R0		; transfer R0 contents to R6 instead	
-	ADD R6, R6, #-9		;
-	BRnz PRINT_Z		;
-	LD R0, A		;
-	ADD R0, R0, #9		;
-	ADD R0, R0, R6		;
-	ADD R0, R0, #-10	;	
-	BR PRINT
+	ADD R6, R6, R0		; R6 <- R0, since R0 will be used for OUT	
+	ADD R6, R6, #-9		; checks if the value <=9
+	BRnz PRINT_NUM		; branches if value <=9
+	LD R0, LETTER		; loads the hex value for 'A'
+	ADD R0, R0, R6		; adds the digit to R0
+	ADD R0, R0, #-10	; adds -10 for letter offset (from flowchart)	
+	BR PRINT		; prints hex digit
 
-PRINT_Z
-	LD R0, ZERO		;
-	ADD R6, R6, #9		;
-	ADD R0, R0, R6		;
+PRINT_NUM
+	LD R0, NUMBER		; Loads the hex value for '0'
+	ADD R0, R0, R6		; adds the digit(R6) as the number offset
 
-PRINT	
-	OUT
+PRINT	ADD R0, R0, #9		; balances back the <=9 check performed
+	OUT			; prints the extracted digit in hex
 	ADD R5, R5, #-1		; decrements R5 after each digit is printed
-	BRp HEX_PRINT		; Goes back to printing next digit
+	BRp DIGIT		; Goes back to printing until 4 digits are printed
 
 NEW_LINE
 	LD R0, NEWL		; loads R0 with the newline character(x000A)
 	OUT			; prints a newline
-	ADD R1, R1, #1		; Points to next letter in alphabet
-	
-	ADD R3, R3, #1		;
-	ADD R2, R2, #-1		; Decrements bin counter
-	BRp BIN_CHAR		; Branches to next char code if bins are remaining
-
-; do not forget to write a brief description of the approach/algorithm
-; for your implementation, list registers used in this part of the code,
-; and provide sufficient comments
-
+	ADD R1, R1, #1		; points to next value in histogram
+	ADD R3, R3, #1		; increments R3 to point to next character
+	ADD R2, R2, #-1		; decrements bin counter
+	BRp BIN_CHAR		; branches to next char code if bins remain
 
 
 DONE	HALT			; done
 
+; the data needed by the hex print part of the program
+NUMBER		.FILL #48	; ASCII '0'
+LETTER		.FILL #65	; ASCII 'A'
+OFFSET		.FILL X0040	; ASCII '@'
+SPACE		.FILL X0020	; ASCII 'Space'
+NEWL		.FILL X000A	; ASCII for new line
 
 ; the data needed by the program
 NUM_BINS	.FILL #27	; 27 loop iterations
@@ -179,11 +195,7 @@ AT_MIN_Z	.FILL xFFE6	; the difference between ASCII '@' and 'Z'
 AT_MIN_BQ	.FILL xFFE0	; the difference between ASCII '@' and '`'
 HIST_ADDR	.FILL x3F00     ; histogram starting address
 STR_START	.FILL x4000	; string starting address
-CHAR_START	.FILL X0040	; ASCII code for '@'
-SPACE		.FILL X0020	; ASCII code for 'space'
-NEWL		.FILL X000A	; ASCII code for 'enter'
-ZERO		.FILL #48	; ASCII code for '0'
-A		.FILL #65	; ASCII code for 'A'
+
 
 
 ; for testing, you can use the lines below to include the string in this
