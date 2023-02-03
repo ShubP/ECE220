b0eff01162b7796ca662beb9286719ee94bd3681
Shub Pereira <shubbp2@illinois.edu>
2023-02-03 02:50:53 -0600

    mp3 update

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,14 +1,30 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-  int row;
+  int row;                                                                               
 
   printf("Enter a row index: ");
   scanf("%d",&row);
 
-  // Write your code here
-
+  if (row==0){
+    printf("1\n");
+  }
+  else{
+    unsigned long int result = 1;
+    unsigned long int k;
+    unsigned long int i;
+    for (k = 0; k <= row; k++){
+      for (i=1; i <= k; i++){
+        result = result * ( row + 1 - i )/(i) ;
+      }
+      printf("%lu ",result);
+      result = 1;
+    }
+    printf("\n");
+  }
   return 0;
 }
