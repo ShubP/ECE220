75d4590f4c17f46a4c292b45d273e8c16c179cb2
Shub Pereira <shubbp2@illinois.edu>
2023-02-03 18:25:54 -0600

    mp3 completed

--- mp3/mp3.c
+++ mp3/mp3.c
@@ -1,30 +1,34 @@
+/* partners: shubbp2, ykko2 */
 
+/* This C program inputs a index row from the user, and prints the binomial coefficients as found in Pascal's triangle. 
+The given equation calculates the coefficient using a for loop. The final result is printed to the display. */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-  int row;                                                                               
+  int row;	
 
-  printf("Enter a row index: ");
-  scanf("%d",&row);
+  printf("Enter a row index: ");	
+  scanf("%d",&row);	
+ 
 
-  if (row==0){
-    printf("1\n");
-  }
-  else{
-    unsigned long int result = 1;
-    unsigned long int k;
-    unsigned long int i;
-    for (k = 0; k <= row; k++){
-      for (i=1; i <= k; i++){
-        result = result * ( row + 1 - i )/(i) ;
-      }
-      printf("%lu ",result);
-      result = 1;
-    }
-    printf("\n");
-  }
+if (row ==0){	/*Case for index '0' */
+	printf("1 ");	/*Print '1' for case '0'*/
+}
+else{
+	unsigned long int result=1;	/* initialize coefficient variable */
+	int k; /* initialize coefficient counter */
+	int i; /* initialize equation counter */
+	for (k=0; k<= row; k++){	/*loop for each coefficient */
+		for (i=1; i<=k; i++){	/* loop for coefficient equation */
+			result =result*(row + 1-i)/(i);	/*calculate using the equation */
+		}
+		printf("%lu ", result);	/* print the coefficient */
+		result =1; 	/* reset coeffient variable */
+	}
+}
+	printf("\n");	/* print next line */
   return 0;
 }
