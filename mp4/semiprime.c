513c3e9b4e534c798aa45f0f134c5dccadcc83bb
Shub Pereira <shubbp2@illinois.edu>
2023-02-08 23:37:25 -0600

    mp4 completed

--- mp4/semiprime.c
+++ mp4/semiprime.c
@@ -1,3 +1,13 @@
+/*
+partners: ykko2, mamir6
+
+The first error was that the semicolon was missing in the return line (second to the last line from main.c). The effect of this error is that the file wouldn't compile.
+In addition, we had to change both return value in the is_prime function. The effect of the return value is that it would return 1 if the number is not prime and 0 if it is prime when it should be the opposite. 
+The second error was in the is_prime(j) if statement. Instead of the module sign, we were supposed to have the division sign because we had to check whether the provided number is evenly divisible by any number between 2 and one less than the provided number. 
+The effect of this error was that it was printing out the incorrect program output. Lastly, the last error was that we had to add "ret=1" and "break" for the is_prime(k) if statement. 
+The effect without the "break" statement is that it would print the last value twice when it should be printed only once. Also, without the "ret=1" statement, it would return the value 0 when it should return the value 1. 
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -31,14 +41,14 @@ int print_semiprimes(int a, int b)
     int ret = 0;
     for (i = a; i <=b; i++) { //for each item in interval
         //check if semiprime
-        for (j = 2; j < i; j++) {
+        for (j = 2; j <= i; j++) {
             if (i%j == 0) {
                 if (is_prime(j)) {
-                    k = i%j;
-                    if (is_prime(i/j) && (k==0)) {
+                    k = i/j;
+                    if (is_prime(k)) {
                         printf("%d ", i);
-                        ret = 1;
-                        break;
+		                ret=1; 
+			            break;
                     }
                 }
             }
