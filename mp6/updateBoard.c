fd3dea6a9a90be70305283330aba4e0e52bbabaf
Shub Pereira <shubbp2@illinois.edu>
2023-02-27 23:24:22 -0600

    mp6 first

--- mp6/updateBoard.c
+++ mp6/updateBoard.c
@@ -1,3 +1,4 @@
+
 /*
  * countLiveNeighbor
  * Inputs:
@@ -13,7 +14,26 @@
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
+    
+    int live = 0;
+    int i,j;
+    for (i=(row-1); i<=(row+1);i++){
+        if (i < boardRowSize && i >= 0){
+            for (j=(col-1); j<=(col+1);j++){
+                if (j < boardColSize && j >= 0){
+                    if (i!=row || j!= col){
+                        int cell_location = (i*boardColSize) + j;
+                        if (board[cell_location]==1){
+                            live++;
+                        } 
+                    }
+                }
+            }
+        }
+    }
+    return live;
 }
+
 /*
  * Update the game board to the next step.
  * Input: 
@@ -24,6 +44,29 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+    int k,l,m, livecount;
+    int newboard[boardRowSize * boardColSize];
+    for (k=0;k < boardRowSize; k++){
+        for (l=0;l < boardColSize; l++){
+            int loc = (k*boardColSize) + l;
+            livecount = countLiveNeighbor(board, boardRowSize, boardColSize, k, l);
+            if (*(board+loc)==0 && (livecount == 3)){
+                newboard[loc] = 1;
+            }
+            else if (*(board+loc)==1 && (livecount<2 || livecount >3)){
+                newboard[loc]=0;
+            }
+            else if (*(board+loc)==1 && (livecount==2 || livecount==3) ){
+                newboard[loc] = 1;
+            }
+            else{
+                newboard[loc]=0;
+            }
+        }
+    }
+    for (m=0; m < (boardRowSize * boardColSize); m++){
+        *(board+m) = newboard[m];
+    }
 }
 
 /*
@@ -36,11 +79,26 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * Output: return 1 if the alive cells for next step is exactly the same with 
  * current step or there is no alive cells at all.
  * return 0 if the alive cells change for the next step.
- */ 
-int aliveStable(int* board, int boardRowSize, int boardColSize){
+ */
+int aliveStable (int* board, int boardRowSize, int boardColSize) {
+    int sameboard[boardRowSize * boardColSize];
+    int y,z;
+    for (y=0;y < boardRowSize; y++){
+        for (z=0;z < boardColSize; z++){
+            int sameloc = y*boardColSize + z;
+            sameboard[sameloc] = board[sameloc];
+        }
+    }
+
+    updateBoard(sameboard, boardRowSize, boardColSize);
+    
+    for (y=0;y < boardRowSize; y++){
+        for (z=0;z < boardColSize; z++){
+            int sameloc = y*boardColSize + z;
+            if (sameboard[sameloc] != board[sameloc]){
+                return 0;
+            }
+        }
+    }
+    return 1;
 }
-
-				
-				
-			
-
