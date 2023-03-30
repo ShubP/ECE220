42d4f9dc6bcef68f1c65d8b6f6583b2a95e7b532
Shub Pereira <shubbp2@illinois.edu>
2023-03-30 16:17:07 -0500

    mp9 final

--- mp9/maze.c
+++ mp9/maze.c
@@ -13,44 +13,44 @@
 maze_t * createMaze(char * fileName)
 {
     // Your code here. Make sure to replace following line with your own code.
-    maze_t * mymaze = (maze_t*) malloc(sizeof(maze_t)); 
-    FILE *file = fopen (fileName, "r");
-    int rows, cols;
-    fscanf (file, "%d %d\n" , &cols, &rows );
-    mymaze -> width = cols;
-    mymaze -> height= rows; 
-    mymaze -> cells = (char **)calloc(rows, sizeof(char *));
-    int i;
-    for (i=0; i<rows; i++)
+    maze_t * mymaze = (maze_t*) malloc(sizeof(maze_t)); //reserve space for memory 
+    FILE *file = fopen (fileName, "r"); //create an instance that stores maze file
+    int rows, cols; // intialize variable 
+    fscanf (file, "%d %d\n" , &cols, &rows); // read the stored size of the maze from the file 
+    mymaze -> width = cols; // assign the width using cols 
+    mymaze -> height= rows;  // assign the height using rows
+    mymaze -> cells = (char **)calloc(rows, sizeof(char *)); //reserve space for each rows
+    int i; // intialize variable 
+    for (i=0; i<rows; i++) // go through every row 
     {
-        mymaze -> cells[i] = (char *)calloc (cols, sizeof(char));
+        mymaze -> cells[i] = (char *)calloc (cols, sizeof(char));//reserve space for each cols
     }
-    int k, l;
-    for ( k=0; k< rows; k++)
+    int k, l;// intialize variable 
+    for ( k=0; k< rows; k++)// go through every row
     {
-        for (l=0; l< cols+1; l++)
+        for (l=0; l< cols+1; l++)// go through every cols
         {
-            char cell = fgetc(file);
-            if (cell == 'S')
+            char cell = fgetc(file); //get the next charcter 
+            if (cell == 'S') //cell is found with 'S'
             {
-                mymaze -> startRow = k;
-                mymaze -> startColumn = l;
+                mymaze -> startRow = k;//assign the row value of 'S'
+                mymaze -> startColumn = l;//assign the col value of 'S'
 
             }
-            else if (cell == 'E')
+            else if (cell == 'E')//cell is found with 'E'
             {
-                mymaze -> endRow = k;
-                mymaze -> endColumn = l;
+                mymaze -> endRow = k;//assign the row value of 'E'
+                mymaze -> endColumn = l;//assign the col value of 'E'
                 
             }
-            if (cell != '\n')
+            if (cell != '\n') // if cell not S or E or next line charcter 
             {
-                mymaze -> cells[k][l] = cell;
+                mymaze -> cells[k][l] = cell; //assign the same character 
             }
         }
     }
-    fclose(file);
-    return mymaze;
+    fclose(file); // close the files
+    return mymaze; //return the created maze 
 }
 
 /*
@@ -63,13 +63,12 @@ maze_t * createMaze(char * fileName)
 void destroyMaze(maze_t * maze)
 {
     // Your code here.
-    int i;
-    for(i=0; i<maze -> height; i++ ){
-        free(maze -> cells[i]);
+    int i;// intialize variable 
+    for(i=0; i<maze -> height; i++ ){ // go through every rows in the maze 
+        free(maze -> cells[i]); //remove the allocation memory 
     }
-    free(maze->cells);
-    free(maze);
-    return;
+    free(maze->cells); // remove all the cells in the maze 
+    free(maze); // remove allocation in the maze 
 }
 
 /*
@@ -84,14 +83,13 @@ void destroyMaze(maze_t * maze)
 void printMaze(maze_t * maze)
 {
     // Your code here.
-    int i,j;
-    for(i=0; i<maze -> width; i++){
-        for(j=0; j<maze -> height; j++){
-            printf("%c",maze -> cells[i][j]);
+    int i,j;// intialize variable 
+    for(i=0; i<maze -> width; i++){ // go through every cols in the maze
+        for(j=0; j<maze -> height; j++){// go through every rows in the maze
+            printf("%c",maze -> cells[i][j]); //assign cells of each rows and cols of the maze 
         }
-        printf("\n");
+        printf("\n"); //print next line 
     }
-    return;
 }
 /*
  * solveMazeDFS -- recursively solves the maze using depth first search,
@@ -105,32 +103,33 @@ void printMaze(maze_t * maze)
 int solveMazeDFS(maze_t * maze, int col, int row)
 {
     // Your code here. Make sure to replace following line with your own code.
-    if ((col <0) || (row <0) || (col > maze -> width) || (row > maze ->height)){
-        return 0;
+    if ((col <0) || (row <0) || (col > maze -> width) || (row > maze ->height)){ //if row, col outside bounds of the maze
+        return 0;//return false
     }
-    if (maze->cells[row][col] == 'E'){
-        return 1;
+    if (maze->cells[row][col] == 'E'){// if row, col is the end of the maze 
+        maze->cells[maze->startRow][maze->startColumn] = 'S'; //replace the starting position asterisk with S once 'E' is found
+        return 1;//return true
     }
-    if (maze->cells[row][col] != ' ' && maze->cells[row][col] != 'S'){
-        return 0;
+    if (maze->cells[row][col] != ' ' && maze->cells[row][col] != 'S'){ // if row,col is not an empty cell and it is not the start
+        return 0;//return false
     }
-    if(maze->cells[row][col] == ' '){
-        maze->cells[row][col] = '*';
+    if(maze->cells[row][col] == ' ' || maze->cells[row][col] == 'S'){// if row,col is an empty cell
+        maze->cells[row][col] = '*';//set row,col as part of solution
     }
-    if (solveMazeDFS(maze, col+1,row) == 1){
-        return 1;
+    if (solveMazeDFS(maze, col-1,row) == 1 ){ //recursively check for cells to the left
+        return 1;//return true
     }
-    if (solveMazeDFS(maze, col,row-1) == 1){
-        return 1;
+    if (solveMazeDFS(maze, col+1,row) == 1 ){//recursively check for cells to the right
+        return 1;//return true
     }
-    if (solveMazeDFS(maze, col,row+1) == 1){
-        return 1;
+    if (solveMazeDFS(maze, col,row-1) == 1 ){//recursively check for cells above
+        return 1;//return true
     }
-    if (solveMazeDFS(maze, col-1,row) == 1){
-        return 1;
+    if (solveMazeDFS(maze, col,row+1) == 1 ){//recursively check for cells below
+        return 1;//return true
     }
-    if(maze->cells[row][col] != 'S'){
-       maze->cells[row][col] = '~'; 
+    if(maze->cells[row][col] != 'S'){//if the cell is not the start
+       maze->cells[row][col] = '~';// set cell as visited but not solution cell
     }
-    return 0;
+    return 0;//return false
 }
